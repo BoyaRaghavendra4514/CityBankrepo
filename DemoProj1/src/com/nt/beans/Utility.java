791c5cff509a1493cfccc02bacea2bce2b78af35
TL <Tl@gmail.com>
2024-10-07 09:16:00 +0530

    adding the few lines in utility.java

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class Utility {
    public Date getSysDate() {
 	   System.out.println("writing the present date and time ");
+	   System.out.println("Writing the System date and time");
 	   return new Date();
    }
 }
